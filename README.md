0a581a62b128aacb7fdb29b4b3085fb27c992046
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-08 01:33:33 -0800

    implement workaround for multiline key

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # GPGMail
 
 Safari extension that adds GPG capabilities to Gmail
+
+## Adding your Key
+
+Safari extensions are not allowed to access the hard disk, so you must provide your key manually.
+
+Add your key and passphrase in Preferences > Extensions > GPGmail
+
+You can get your key into your clipboard with this command
+
+    gpg --export-secret-key -a "Keyvan Fatehi" | pbcopy
+
+## Decrypting Emails
+
+Go to Gmail and you'll notice a new "Decrypt" button. Use that on any PGP message your private key can decipher.
--- build/global.js
+++ build/global.js
@@ -192,10 +192,23 @@
 /* 10 */
 /***/ function(module, exports) {
 
-	// gpg --export-secret-key -a "Keyvan Fatehi" | openssl base64 | pbcopy
+	// gpg --export-secret-key -a "Keyvan Fatehi" | pbcopy
 	module.exports.getPrivateKey = function() {
-	  var base64Key = safari.extension.secureSettings.privateKey
-	  return atob(base64Key);
+	  // safari doesnt give us multiline string in settings
+	  // it turns every newline into a space
+	  // so we have to fudge around to recreate this thing
+	  var raw = safari.extension.secureSettings.privateKey;
+	  var version = raw.match(/GnuPG v(\d+)/)[1];
+	  var body = raw.match(/  (.+) -----END/)[1].replace(/ /g, '\n')
+	  var lines = [];
+	  lines.push('-----BEGIN PGP PRIVATE KEY BLOCK-----')
+	  lines.push('Version: GnuPG v'+version);
+	  lines.push('');
+	  lines.push(body);
+	  lines.push('-----END PGP PRIVATE KEY BLOCK-----');
+	  var result = lines.join('\n');
+	  console.log(result);
+	  return result;
 	}
 
 	module.exports.getKeyPassphrase = function() {
